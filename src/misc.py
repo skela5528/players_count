713734b267277650923900cb8a905b18ea758ef7
skela5528 <skela@yandex.ru>
2021-06-20 14:36:04 +0200

    misc.py move draw_boxes()

--- src/misc.py
+++ src/misc.py
@@ -1,5 +1,10 @@
 import logging
 import os
+from typing import Optional, List
+
+import cv2
+import numpy as np
+from PIL import Image
 
 
 def get_logger(log_path: str = None, level=logging.INFO) -> logging.Logger:
@@ -23,3 +28,24 @@ def get_logger(log_path: str = None, level=logging.INFO) -> logging.Logger:
 
 
 LOGGER = get_logger()
+
+
+COLORS = {0: (255, 255, 255),
+          1: (250, 0, 0),
+          2: (0, 0, 250),
+          10: (0, 0, 0)}
+
+
+def draw_boxes(img: Image, boxes: list, texts: Optional[List[str]] = None, color: tuple = (0, 0, 250),
+               classes: Optional[list] = None) -> np.ndarray:
+    img = np.array(img)
+    for ind, box in enumerate(boxes):
+        left, top, right, bottom = [int(round(val)) for val in box]
+        if classes is not None:
+            color = COLORS.get(classes[ind])
+        cv2.rectangle(img, (left, top), (right, bottom), color, 2)
+
+        if texts is not None:
+            text_org = (left + 12, top + 32)
+            cv2.putText(img, texts[ind], text_org, cv2.FONT_HERSHEY_DUPLEX, 1, color, 2)
+    return img
